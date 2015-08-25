572465a70b4046029ee81d72e09b3058a257e009
Abhilash-Mandlekar <abhilash.mandlekar@gmail.com>
2015-08-25 18:37:33 +0530

    save button works!!!
    This takes the input from subject of reminder and displays on screen
    which is a list view.

--- app/src/main/java/com/example/abhilash/reminder/MainActivity.java
+++ app/src/main/java/com/example/abhilash/reminder/MainActivity.java
@@ -1,18 +1,45 @@
 package com.example.abhilash.reminder;
 
 import android.os.Bundle;
+import android.content.ClipData;
+import android.content.Context;
+import android.database.DataSetObserver;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static ArrayList<String> reminders =new ArrayList<String>() ;
+    public static String sub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 
     @Override
@@ -24,8 +51,23 @@ public class MainActivity extends ActionBarActivity {
 
     public void addReminder(View view)
     {
-        //TextView str= (TextView) findViewById(R.id.welcome_layout);
+        //TextView str= (TextView) findViewById(R.id.hello_text_view);
         setContentView(R.layout.reminder_ui);
+    }
+
+    public void saveReminder(View view)
+    {
+        EditText text = (EditText) findViewById(R.id.subject);
+        sub = text.getText().toString();
+        reminders.add(sub);
+        /*//setContentView(R.layout.text_view_reminder);
+        ArrayAdapter rem = new ArrayAdapter<String>(this,R.layout.text_view_reminder, R.id.list_item,reminders);
+        ListView lv = (ListView) findViewById(R.id.list_item);
+        lv.setAdapter( rem);*/
+        setContentView(R.layout.dummy_layout);
+        getFragmentManager().beginTransaction()
+                .add(R.id.container,new ListReminder())
+                .commit();
     }
 
     @Override
@@ -42,4 +84,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
